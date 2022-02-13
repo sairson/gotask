32849a0ec28734a9c80a9201e43140f6f576e107
SaiRson <74412075+sairson@users.noreply.github.com>
2022-02-13 17:24:20 +0800

    Add files via upload

--- example/main.go
+++ example/main.go
@@ -13,14 +13,14 @@ func AddFunc(a,b int) int {
 func main(){
 	//service.Debug(false)
 
-	err := TaskConntroller.InitAsync(100, 3, "192.168.248.219", 6379, "")
+	err := gotask.InitAsync(100, 3, "192.168.248.219", 6379, "")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 	time.Sleep(6 * time.Second)
-	TaskConntroller.Register("add",AddFunc) // 注册函数
-	send, err := TaskConntroller.Invoke("add", "这是测试函数add",[]map[string]interface{}{
+	gotask.Register("add",AddFunc) // 注册函数
+	send, err := gotask.Invoke("add", "这是测试函数add",[]map[string]interface{}{
 		{"int": 1},
 		{"int": 2},
 	}...) // 调用函数
@@ -28,5 +28,5 @@ func main(){
 		return
 	}
 	fmt.Println(send)
-	TaskConntroller.Wait(3 * time.Second)
+	gotask.Wait(3 * time.Second)
 }
